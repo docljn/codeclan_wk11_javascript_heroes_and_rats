54966eb8111ed774308bb764f1e8fb37e168a74a
DocLJN <ljnoble@gmail.com>
2018-02-16 13:52:12 +0000

    Hero has a name

---
+++ specs/hero_specs.js
@@ -0,0 +1,30 @@
+/*global describe, beforeEach, it*/
+/*
+A. Create a constructor to create a Hero character
+
+- A Hero has a name
+- A Hero has health
+- A Hero has a favourite food
+- A Hero can talk saying their name
+- A Hero has a collection of tasks to complete
+*/
+
+const Hero = require("../hero.js");
+const assert = require("assert");
+
+describe("Hero", function () {
+
+  let hero; //means variable is scoped to describe
+
+  beforeEach( function () {
+    hero = new Hero("Conan");
+  });
+
+  it("should have a name", function () {
+    const actual = hero.name;
+    assert.strictEqual(actual, "Conan");
+  });
+
+
+
+});
