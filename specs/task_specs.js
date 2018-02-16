9719d23331409e52062ccc03e2e415bc43947fdd
DocLJN <ljnoble@gmail.com>
2018-02-16 15:11:04 +0000

    Task has difficulty level between 1[easiest] and 5[hardest]

--- specs/task_specs.js
+++ specs/task_specs.js
@@ -18,12 +18,30 @@ describe("Task", function () {
   let task;
 
   beforeEach( function () {
-    task = new Task("Clean the Augean stables");
+    task = new Task("Clean the Augean stables", 5);
   });
 
   it("should have a description", function () {
     const actual = task.description;
     assert.strictEqual(actual, "Clean the Augean stables");
   });
+
+  it("should have a difficulty level between 1[easiest] and 5[hardest]", function () {
+    const actual = task.getDifficultyLevel();
+    assert.strictEqual(actual, "hardest");
+  });
+
+  it("should should treat a difficulty of greater than 5 as 5", function () {
+    const task = new Task ("breathe underwater", 15);
+    const actual = task.getDifficultyLevel();
+    assert.strictEqual(actual, "hardest");
+  });
+
+  it("should should treat a difficulty of less than 1 as 1", function () {
+    const task = new Task ("blink", -15);
+    const actual = task.getDifficultyLevel();
+    assert.strictEqual(actual, "easiest");
+  });
+
 
 });
--- task.js
+++ task.js
@@ -1,5 +1,39 @@
-const Task = function (description) {
+const DifficultyEnum = {
+  HARDEST: 5,
+  HARD: 4,
+  AVERAGE: 3,
+  EASY: 2,
+  EASIEST: 1,
+  properties: {
+    1: {name: "easiest", value: 1},
+    2: {name: "easy", value: 2},
+    3: {name: "average", value: 3},
+    4: {name: "hard", value: 4},
+    5: {name: "hardest", value: 5}
+  }
+};
+
+Object.freeze(DifficultyEnum);
+
+const Task = function (description, difficulty) {
   this.description = description;
+  this.difficulty = difficulty;
+};
+
+/*
+https://stijndewitt.com/2014/01/26/enums-in-javascript/
+*/
+
+
+Task.prototype.getDifficultyLevel = function () {
+  if (this.difficulty < 1) {
+    this.difficulty = 1;
+  }
+  if (this.difficulty > 5) {
+    this.difficulty = 5;
+  }
+
+  return DifficultyEnum.properties[this.difficulty].name;
 };
 
 
